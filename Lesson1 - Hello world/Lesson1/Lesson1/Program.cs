f6bfae73417041a2a7455e8b153ae7e643dae878
giloxin <giloxin@gmail.com>
2020-12-14 22:04:02 +0300

    Added first lesson

---
+++ Lesson1 - Hello world/Lesson1/Lesson1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте, молодой человек! Как я могу к Вам обращаться?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Today.ToString("d")}, {DateTime.Today.DayOfWeek}");
+        }
+    }
+}
